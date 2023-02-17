1ba63400cca4891cbca70c3329a30fa8b5bb69cd
Ekow20 <ekown20@gmail.com>
2023-02-17 13:58:14 +0000

    hello world c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*Main is the Entry Point
+returning 0 */
+
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*Main is the Entry Point
+returning 0 */
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
