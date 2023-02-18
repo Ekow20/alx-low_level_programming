db4833aefc3a406a4a1f3b6560ff6797d5ca46a7
Ekow20 <ekown20@gmail.com>
2023-02-18 01:15:07 +0000

    if else c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,11 @@
 *main - Entry Point
 *Return: Always 0 (Success)
 */
-int main (void) {
+int main(void)
+{
 char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++) 
+for (ch = 'a' ; ch <= 'z' ; ch++)
 putchar(ch);
 putchar('\n');
-return(0);
+return (0);
 }
