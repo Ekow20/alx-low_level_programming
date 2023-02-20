5dedc67fffed527b38c231a67cb25ac51eb83241
Ekow20 <ekown20@gmail.com>
2023-02-20 01:01:47 +0000

    if else c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ for (num3 = num2 + 1; num3 <= 9; num3++)
 putchar(num1 + '0');
 putchar(num2 + '0');
 putchar(num3 + '0');
-if(num1 != 7 || num2 != 8 || num3 != 9)
+if (num1 != 7 || num2 != 8 || num3 != 9)
 {
 putchar(',');
 putchar(' ');
