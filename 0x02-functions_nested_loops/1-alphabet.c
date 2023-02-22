d9fe10b51081e8e4218d3ab4200b447b82430cc9
Ekow20 <ekown20@gmail.com>
2023-02-22 13:04:44 +0000

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*print_alphabet -
+*print_alphabet - prints the alphabet, in lowercase
 */
-char ch;
 void print_alphabet(void)
 {
+char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * print_alphabet_x10 
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
-char ch;
-int i;
 void print_alphabet_x10(void)
 {
+char ch;
+int i;
 for ( i = 0; i < 10; i++)
 {   
 for (ch = 'a'; ch <= 'z'; ch++)
