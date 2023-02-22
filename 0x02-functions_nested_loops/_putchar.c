25cff3cf293c0b429d30930e4fba6d271f620351
Ekow20 <ekown20@gmail.com>
2023-02-22 12:40:19 +0000

    nested loops

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
@@ -9,20 +10,4 @@
 int _putchar(char c)
 {
 return (write(1, &c, 1));
-}
-/**
-*main - Entry Point
-*Return: Always 0 (Success)
-*/
-char ch;
-void print_alphabet(void)
-{
-for (ch = 'a'; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
-}
-int main(void)
-{
-print_alphabet();
-return (0);
 }
