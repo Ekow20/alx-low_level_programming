f0d1646d053453857418ed3d5af993db1aa4ece0
Ekow20 <ekown20@gmail.com>
2023-02-23 00:48:07 +0000

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,8 +11,14 @@ while (f < 10)
 int l = 0;
 while (l < 10)
 {
-int a =(f*l); 
+int a =(f * l); 
+if (a < 10)
 _putchar(a + '0');
+else
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+}
 if (l != 9)
 {
 if (a >= 10)
