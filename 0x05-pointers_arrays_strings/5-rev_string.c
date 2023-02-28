836c67557247896a5252062408affa02370c14ab
Ekow20 <ekown20@gmail.com>
2023-02-28 13:26:21 +0000

    pointers arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,19 @@
 #include "main.h"
 #include <string.h>
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
 
 void rev_string(char *s)
 {
-    int len = strlen(s);
-    int i;
-    for (i = 0; i < len / 2; i++)
-    {
-        char temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+int len = strlen(s);
+int i;
+for (i = 0; i < len / 2; i++)
+{
+char temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,2 +1,15 @@
 #include "main.h"
-
+/**
+ * puts2 - function ery other character of a string
+ * @str: input
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
+}
