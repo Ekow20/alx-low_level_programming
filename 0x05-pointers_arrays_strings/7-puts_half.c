29607e64cf4d4b0a2599a18e0199130f5d74edfe
Ekow20 <ekown20@gmail.com>
2023-02-28 14:18:26 +0000

    pointers arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,10 @@
 void puts_half(char *str)
 {
 int len = strlen(str);
+int n = (len + 1) / 2;
 int i;
-if (len % 2 == 0)
+for (i = n; i < len; i++)
 {
-for (i = len / 2; i < len ; i++)
-_putchar(str[i]);
-}
-else
-{
-for (i = (len - 1) / 2; i < len ; i++)
 _putchar(str[i]);
 }
 _putchar('\n');
