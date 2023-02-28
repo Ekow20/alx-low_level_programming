bc95b6a63329bcdd690d564dbf5958b11ecd3b06
Ekow20 <ekown20@gmail.com>
2023-02-28 00:01:42 +0000

    pointers arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,16 @@
 #include "main.h"
-#include <string.h>
-
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
 int _strlen(char *s)
 {
-char str[] = *s; 
-return (strlen(str));
-
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,23 @@
 #include "main.h"
-
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+int i;
+s--;
+for ( i = len; i > 0; i--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
