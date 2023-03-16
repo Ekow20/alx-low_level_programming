19e3a7b7ed0e36082bb2fc2de9003abb1b913485
Ekow20 <ekown20@gmail.com>
2023-03-16 00:03:51 +0000

    argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+if (argc == 3)
+{
+int ans;
+ans = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n",ans);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
