e0830c72c84b19dc30ee302a562bada529a5560d
Ekow20 <ekown20@gmail.com>
2023-03-16 00:18:52 +0000

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
 
 int main(int argc, char *argv[])
 {
 if (argc == 3)
 {
 int ans;
-ans = atoi(argv[1]) * atoi(argv[2]);
+ans = (atoi(argv[1]) * atoi(argv[2]));
 printf("%d\n",ans);
 }
 else
