f51d3fc1d588222c5ee3c86cdf276cfd4fa79b5f
Ekow20 <ekown20@gmail.com>
2023-03-23 23:38:37 +0000

    variadic func

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
-int sum = 0;
-int i;
+unsigned int sum = 0;
+unsigned int i;
 va_start(args, n);
 for(i=0; i < n; i++)
 {
 int x = va_arg(args, int);
 sum += x;
 }
-return (sum);
 va_end(args);
+return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,20 +7,17 @@
  * @n: The number of integers passed to the function.
  * @...: A variable number of numbers to be printed.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-if (separator==NULL)
-return;
 va_list args;
 unsigned int i;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-int x = va_arg(args, int);
-if (n - i != 1)
-printf("%d%s", x, separator);
-else
-printf("%d\n", x);
+printf("%d", va_arg(args, int));
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
 }
+printf("\n");
+va_end(args);
 }
