c817b5fa9ece43c1adb335db9e20cfbd1dc0e5ca
Ekow20 <ekown20@gmail.com>
2023-03-23 23:25:43 +0000

    variadic func

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ * Return: sum of all parameters.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return (0);
+va_list args;
+va_start(args, n);
+
+int sum = 0;
+int i;
+for(i=0; i < n; i++)
+{
+int x = va_arg(args, int);
+sum += x;
+}
+return (sum);
+va_end(args);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+if (separator==NULL)
+return;
+va_list args;
+va_start(args, n);
+int i;
+for (i = 0; i < n; i++)
+{
+int x = va_arg(args, int);
+if (n - i != 1)
+printf("%d%s", x, separator);
+else
+printf("%d\n", x);
+}
+}
