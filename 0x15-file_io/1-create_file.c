f28667187053bfe5f747a1c0cc2f7cd46da5f991
Ekow20 <ekown20@gmail.com>
2023-04-10 16:59:30 +0000

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,26 +8,26 @@
  * Return: If the function fails - -1.
  * Otherwise - 1.
  */
-int create_new_file(const char *filename, char *text)
+int create_file(const char *filename, char *text_content)
 {
-int file_descriptor, bytes_written, text_length = 0;
+int fd, w, length = 0;
 
 if (filename == NULL)
 return (-1);
 
-if (text != NULL)
+if (text_content != NULL)
 {
-while (text[text_length])
-text_length++;
+for (length = 0; text_content[length];)
+length++;
 }
 
-file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-bytes_written = write(file_descriptor, text, text_length);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(fd, text_content, length);
 
-if (file_descriptor == -1 || bytes_written == -1)
+if (fd == -1 || w == -1)
 return (-1);
 
-close(file_descriptor);
+close(fd);
 
 return (1);
 }
