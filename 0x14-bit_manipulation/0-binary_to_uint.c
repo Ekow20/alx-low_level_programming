d69fd3490670e64f2375ff644fb5d60ac4c6db83
Ekow20 <ekown20@gmail.com>
2023-04-10 23:38:48 +0000

    file i/o

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary string to an unsigned integer
+ * @b: The binary string to convert
+ *
+ * Return: The unsigned integer equivalent of the binary string,
+ *         or 0 if the input string is NULL or not a valid binary string
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int decimal_value = 0;
+
+if (b == NULL) 
+{
+return 0;
+}
+
+for (int i = 0; b[i] != '\0'; i++)
+{
+char current_char = b[i];
+if (current_char < '0' || current_char > '1') {
+return 0;
+}
+decimal_value = decimal_value * 2 + (current_char - '0');
+}
+
+return (decimal_value);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of an unsigned long int
+ * @n: The unsigned long int to print
+ *
+ * This function prints the binary representation of an unsigned long int
+ * to the standard output using the _putchar function.
+ */
+void print_binary(unsigned long int n)
+{
+int counter = 0;
+int i;
+for (i = 63; i >= 0; i--)
+{
+unsigned long int current = n >> i;
+
+if (current & 1)
+{
+_putchar('1');
+counter++;
+}
+else if (counter)
+{
+_putchar('0');
+}
+}
+
+if (!counter)
+{
+_putchar('0');
+}
+}
